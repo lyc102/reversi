abf3e8228e91af037df4ab77e991c1dc6429071f
ifem <lyc102@gmail.com>
2019-05-13 21:13:41 -0700

    add AI vs AI and two gif games

--- AIrand.m
+++ AIrand.m
@@ -1,22 +1,27 @@
-function [u,currentColor,pass] = AIrand(u,currentColor)
+function [u,currentColor,pass] = AIrand(u,currentColor,pass)
 %% AIRAND randomly put a stone
 %
 % This is the simplest move strategy: randomly chose a valid location.
 %
 % Long Chen 2019. May. 12.
 
-p = find(u(:) == 0);
+%% Find all empty space
+p = find(u(:) == 0); % find empty space
 if isempty(p) % no empty space
     pass = 2;
     return
 end
+%% Permute location and randomly put astone
 p = p(randperm(length(p)));
-pass = 1;
+pass = pass + 1;
 for i = 1:length(p)
     [~,~,flipNum] = putstone(u,p(i),currentColor,0); 
     if flipNum
         [u,currentColor,flipNum] = putstone(u,p(i),currentColor); 
         pass = 0;
-        break
+        return
     end
 end
+%% Pass
+% No valid move, pass = 1 and reverse the color
+currentColor = - currentColor;
--- ReversiAI2AI.m
+++ ReversiAI2AI.m
@@ -18,28 +18,20 @@ u(5,5) = 1;
 u(4,5) = -1;
 u(5,4) = -1;
 plotgame(u);
+gif('reversidemo.gif') 
 
 %% Play the game
 currentColor = 1; % start from black 
 h = 1/8;
 pass = 0; 
-while pass < 2 % exit with two consective pass 
-    [x,y] = myginput(1,'circle');    
-    j = round(x/h-0.5)+1;
-    i = round(y/h-0.5)+1;
-    if (i<1) || (i>8)|| (j<1) || (j>8) % click out of the board is pass
-        pass = pass + 1;
-        currentColor = - currentColor;
-        continue;
-    end
-    p = sub2ind([8,8],i,j);
-    if u(i,j) == 0 % no stone
-        % put the stone and reverse stones captured
-        [u,currentColor,flipNum] = putstone(u,p,currentColor);
-        if flipNum
-            [u,currentColor,pass] = AIrand(u,currentColor,pass); 
-        end
-    end
+while pass < 2 % exit with pass = 1
+    % put the stone and reverse stones captured
+    [u,currentColor,pass] = AIrand(u,currentColor,pass); 
+    pause(1)
+    gif;
+    [u,currentColor,pass] = AIrand(u,currentColor,pass);        
+    pause(1)
+    gif;
 end
 
 %% Count 
